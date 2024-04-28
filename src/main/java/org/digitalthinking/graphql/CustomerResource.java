9866458377b3a758fd55e162d3dec924ec7ae8ec
joffre <joffre.hermosilla@gmail.com>
2024-04-27 22:17:08 -0500

    chore[0]: configuracion GraphQL Api Reactivo

---
+++ src/main/java/org/digitalthinking/graphql/CustomerResource.java
@@ -0,0 +1,15 @@
+package org.digitalthinking.graphql;
+
+import io.smallrye.mutiny.Uni;
+import org.digitalthinking.entites.Customer;
+import org.digitalthinking.repositories.CustomerRepository;
+import org.eclipse.microprofile.graphql.*;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@GraphQLApi
+public class CustomerResource {
+
+
+}
